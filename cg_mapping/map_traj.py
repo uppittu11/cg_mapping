1aa9d9681b84a17b3de962f2bfa5ff39a5380d71
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-06-21 20:45:44 -0500

    List comprehension for mapping dictionary

--- cg_mapping/map_traj.py
+++ cg_mapping/map_traj.py
@@ -3,11 +3,37 @@ import numpy as np
 import mdtraj
 from collections import OrderedDict
 
+def _load_mapping(mapfile=None):
+    """ Load a forward mapping
+    
+    Parameters
+    ----------
+    mapfile : str
+        Path to mapping file
+
+    Returns
+    -------
+    mapping_dict : OrderedDict()
+        OrderedDict (CG bead: list of atom indices)
+
+        """
+
+    mapping_dict = OrderedDict()
+    with open(mapfile,'r') as f:
+        mapping_dict = {line.split(":")[0].rstrip(): line.split(":")[1].rstrip().split()
+                        for line in f if line.rstrip()}
+    return mapping_dict
+
+
+
+
 #trajfile = "md_pureDSPC.xtc"
 trajfile = "md_pureDSPC.pdb"
 traj = mdtraj.load(trajfile)
 
 # Read in the mapping file
+mapfile = 'mappings/DSPC.map'
+mapping_dict = _load_mapping(mapfile=mapfile)
 
 # Go through the trajectory frame by frame
 
