2543097320be2eed88a5bd31182c25f652d93fc5
ahy3nz <alexander.h.yang@vanderbilt.edu>
2017-08-30 18:23:24 -0500

    Finish compute rdf, argumnet parsing too

--- cg_mapping/get_bonded_parameters.py
+++ cg_mapping/get_bonded_parameters.py
@@ -1,14 +1,19 @@
 from cg_mapping import *
 import mdtraj
 import itertools
+import argparse
 
 """ Simple script to iterate through all bead types
 and compute bond/angle parameters by fitting to
 Gaussian distributions"""
 beadtypes=['P4', 'P3', 'Nda', 'Na', 'C1', 'Qa', 'Q0']
 
-
-traj = mdtraj.load("bonded_cg-traj.xtc", top="bonded_cg-traj.pdb")
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", dest="trajectory", help="Trajectory")
+parser.add_argumnet("-c", dest="topology", help="File with structure/topology info")
+args = parser.parse_args()
+#traj = mdtraj.load("bonded_cg-traj.xtc", top="bonded_cg-traj.pdb")
+traj = mdtraj.load(args.trajectory, top=args.topology)
 all_atoms = [a.name for a in traj.topology.atoms]
 
 bulk_DSPC_900K = cg_utils.State(k_b=8.314e-3, T=900)
@@ -33,3 +38,8 @@ for x,z in itertools.product(beadtypes,repeat=2):
             angle_parameters = bulk_DSPC_900K.compute_angle_parameters(traj, x, y, z)
             print(angle_parameters)
 
+print("*"*20)
+print("Generating RDFs")
+print("*"*20)
+for x,y in itertools.product(beadtypes,repeat=2):
+    bulk_DSPC_900K.compute_rdf(x,y,"{}-{}-state_A".format(x,y))
