f07097bf40d16cecebbdb3283aec81d18219d6d9
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-04-06 20:42:04 -0500

    Have to generate networkx graph

--- cg_mapping/scripts/get_bonded_parameters.py
+++ cg_mapping/scripts/get_bonded_parameters.py
@@ -11,7 +11,8 @@ import argparse
 """ Simple script to iterate through all bead types
 and compute bond/angle parameters by fitting to
 Gaussian distributions"""
-beadtypes=['P4', 'P3', 'Nda', 'E1', 'C2', 'C3', 'PCP', 'PCN']
+#beadtypes=['P4', 'P3', 'Nda', 'E1', 'C2', 'C3', 'PCP', 'PCN']
+beadtypes=['_CH3', '_CH2']
 #beadtypes=['P4', 'P3', 'Nda', 'Na', 'C2', 'C1', 'Qa', 'Q0']
 
 parser = argparse.ArgumentParser()
