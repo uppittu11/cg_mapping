f547910bdc6ceed6196b5d30861c0d64847b81d6
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2020-05-18 14:53:20 -0700

    Switch test to use new function

--- tests/test_traj.py
+++ tests/test_traj.py
@@ -5,9 +5,7 @@ from conf_test import ConfTest
 class TestTraj(ConfTest):
     def test_construct_traj(self, minimal_mapper):
         minimal_mapper._aa_traj = minimal_mapper._aa_traj[:2]
-        minimal_mapper._map_topology()
-        minimal_mapper._convert_xyz()
-        minimal_mapper._construct_traj()
+        minimal_mapper.cg_map()
         assert minimal_mapper._cg_traj.n_atoms == 460
         assert np.allclose(minimal_mapper._cg_traj.xyz[0,0],
                            [1.0085664, 1.4182341, 3.6780086])
