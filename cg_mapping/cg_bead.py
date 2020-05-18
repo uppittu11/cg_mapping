6ab8a1cceb67a10b706815febfe49408fe5d53b8
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2020-05-18 12:11:46 -0700

    Remove unused CGBead properties

--- cg_mapping/cg_bead.py
+++ cg_mapping/cg_bead.py
@@ -1,21 +1,9 @@
 class CGBead:
     def __init__(self,
-            bead_index=0,
             bead_type=None,
-            res_name=None,
             atom_indices=None):
-        self._bead_index = bead_index
         self._bead_type = bead_type
-        self._res_name = res_name
         self._atom_indices = atom_indices
-
-    @property
-    def bead_index(self):
-        return self._bead_index
-
-    @bead_index.setter
-    def bead_index(self, bead_index):
-        self._bead_index = bead_index
 
     @property
     def bead_type(self):
@@ -32,12 +20,3 @@ class CGBead:
     @atom_indices.setter
     def atom_indices(self, atom_indices):
         self._atom_indices = atom_indices
-
-    @property
-    def res_name(self):
-        return self._res_name
-
-    @res_name.setter
-    def res_name(self, res_name):
-        self._res_name = res_name
-
